65e57504316f185590aff962aa57688b23d69f88
Mark Otuya <markotuya0@gmail.com>
2022-09-15 06:10:32 +0100

    lint

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 function Header() {
-	return (
-		<header>
-			<h1>Marks<strong>Anime</strong>Database</h1>
-		</header>
-	)
+  return (
+    <header>
+      <h1>
+        Marks
+        <strong>Anime</strong>
+        Database
+      </h1>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
