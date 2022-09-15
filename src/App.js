9273be23f2a6fe4b455a0e2cca119a983e606b0c
Mark Otuya <markotuya0@gmail.com>
2022-09-15 22:39:10 +0100

    completed-sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import MainContent from './components/MainContent';
 
 function App() {
+  const [animeList, SetanimeList] = useState([]);
+  const [topAnime,SetTopAnime] = useState([]);
+  const [search, setSearch] = useState('');
+
+  const GetTopAnime = async () => {
+    const temp = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`)
+      .then((res) => res.json());
+
+    SetTopAnime(temp.top.slice(0, 5));
+  }
+
+  useEffect(() => {
+    GetTopAnime();
+  }, []);
+
+
   return (
     <div className="App">
       <Header />
+      <div className="content-wrap">
+        <Sidebar
+          topAnime={topAnime}
+        />
+        <MainContent />
+      </div>
     </div>
   );
 }
---
+++ src/components/MainContent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main>
+      <div
+        className="main-head"
+      >
+        <form className="search-box">
+          <input
+            type="search"
+            placeholder="search for an anime..."
+            required
+          />
+        </form>
+      </div>
+    </main>
+  );
+}
+
+export default MainContent;
